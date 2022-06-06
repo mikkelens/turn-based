427b35709b0fd0841f70d6cc0a25a77ae2742390
Mikkel S <Mikkel123sej@gmail.com>
2022-06-06 22:02:41 +0200

    removed old code

--- Assets/Scripts/Combat/Entities/Enemy.cs
+++ Assets/Scripts/Combat/Entities/Enemy.cs
@@ -3,11 +3,6 @@
     public class Enemy : Entity
     {
         #region Entity specifications/overrides
-        protected override void Die()
-        {
-            base.Die();
-            Fight.Instance.WinBattle(); // should let fightmanager decide this
-        }
         #endregion
     }
 }
--- Assets/Scripts/Combat/Entities/Player.cs
+++ Assets/Scripts/Combat/Entities/Player.cs
@@ -18,7 +18,5 @@ namespace Combat.Entities
 
         #region Entity specifications/overrides
         #endregion
-
-        
     }
 }
